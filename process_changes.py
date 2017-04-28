a4024adee5e88ec4e52b44ee8427994826bbde23
10353830 <10353830@mydbs.ie>
2017-04-28 20:15:03 +0100

    First look inside file
    A look at the count of rows and revisions

--- process_changes.py
+++ process_changes.py
@@ -2,4 +2,23 @@
 changes_file = 'changes_python.log'
 # look at the file
 the_file = open(changes_file, 'r')
-print the_file
+#print the_file
+
+# Look at the data inside the file
+data = the_file.readlines()
+
+#Output the data
+#print data
+
+#Find the count of lines in the file
+print "There are {} lines in the file".format(len(data))
+
+#Find the count of revisions no Class
+sep = 72*'-'
+revision_count = 0
+for line in data:
+	if sep in line:
+		revision_count+=1
+		continue
+print "There are {} revisions in the file".format(revision_count -1)
+
